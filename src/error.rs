22f867b3a78a186ef3ddf39c67d36c13c41d336b
Matthias Geier <Matthias.Geier@gmail.com>
2020-10-25 21:08:56 +0100

    More comments, better examples, 2018 idioms

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,15 @@
 // TODO: Display impls
 
+/// Error type for `Consumer::try_pop()`.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum PopError {
+    /// The queue was empty.
     Empty,
 }
 
+/// Error type for `Producer::try_push()`.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum PushError<T> {
+    /// The queue was full.
     Full(T),
 }
