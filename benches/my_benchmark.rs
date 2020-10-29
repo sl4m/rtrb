3115f174e79e36ca120f549c1090dbcc348d3b9e
Matthias Geier <Matthias.Geier@gmail.com>
2020-10-29 11:11:07 +0100

    use black_box

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,25 +1,34 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-//use criterion::black_box;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rtrb::RingBuffer;
 
 pub fn criterion_benchmark(criterion: &mut Criterion) {
-    criterion.bench_function("push and pop vec", |b| {
-        let mut v = Vec::with_capacity(1);
+    criterion.bench_function("write and read boxed variable", |b| {
+        let mut v = Box::new(666);
         let mut i: usize = 0;
         b.iter(|| {
-            v.push(i);
-            assert_eq!(v.pop(), Some(i));
-            i = i.wrapping_add(1);
+            *v = black_box(i);
+            assert_eq!(*v, black_box(i));
+            i += black_box(1);
         })
     });
 
-    criterion.bench_function("push and pop rb", |b| {
+    criterion.bench_function("push and pop Vec", |b| {
+        let mut v = Vec::with_capacity(1);
+        let mut i: usize = 0;
+        b.iter(|| {
+            v.push(black_box(i));
+            assert_eq!(v.pop(), black_box(Some(i)));
+            i += black_box(1);
+        })
+    });
+
+    criterion.bench_function("push and pop 1-element RingBuffer", |b| {
         let (p, c) = RingBuffer::new(1).split();
         let mut i: usize = 0;
         b.iter(|| {
-            p.push(i).unwrap();
-            assert_eq!(c.pop(), Ok(i));
-            i = i.wrapping_add(1);
+            p.push(black_box(i)).unwrap();
+            assert_eq!(c.pop(), black_box(Ok(i)));
+            i += black_box(1);
         })
     });
 }
